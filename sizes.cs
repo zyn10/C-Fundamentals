2fc1abd59399e2e369247aad6ce857059d283336
Muhammad Zain <95617382+zyn10@users.noreply.github.com>
2022-09-25 18:54:21 +0500

    Create sizes.cs

---
+++ sizes.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Size of Int is: " + sizeof(int));
+
+        Console.WriteLine("Size of cahr is: " + sizeof(char));
+
+        Console.WriteLine("Size of double is: " + sizeof(double));
+
+        Console.WriteLine("Size of float is: " + sizeof(float));
+
+    }
+
+}
